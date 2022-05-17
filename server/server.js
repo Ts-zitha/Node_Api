f71287d8418f26f4d70db4fd6b35816698554769
Ts-zitha <msipho351@gmail.com>
2022-05-17 14:09:30 +0200

    installed cors and created a server

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express =  require('express');
+var cors = require('cors');
+var app = express();
+
+
+var corsOptions = {
+    origin: 'http://localhost:8080'
+}
+console.log('Is our project running');
