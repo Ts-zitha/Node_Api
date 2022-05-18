c1d164b5667aa4f7fb7af7474988f989313446ee
Ts-zitha <msipho351@gmail.com>
2022-05-18 13:19:00 +0200

    server created

--- server/server.js
+++ server/server.js
@@ -6,4 +6,23 @@ var app = express();
 var corsOptions = {
     origin: 'http://localhost:8080'
 }
-console.log('Is our project running');
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+
+//simple route
+
+app.get('/', (req, res)=>{
+    res.json({
+        message : 'Welcome to the World of Node JS'
+    });
+});
+require("../app/routes/route.js")
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT, ()=>{
+    console.log("server running....")
+})
